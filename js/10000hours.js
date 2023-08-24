c43939ceff30d2708ba47870e35a676be2b43cd2
makebeautyNumber <34811087+makebeautyNumber@users.noreply.github.com>
2023-08-24 14:40:24 +0900

    Update 10000hours.js

--- js/10000hours.js
+++ js/10000hours.js
@@ -55,16 +55,9 @@ window.onclick = function (event) {
 
 function copyUrl() {
     let url = window.location.href;
-    let tmp = document.createElement('input');
-    
-
-    document.body.appendChild(tmp);
-    tmp.value = url;
-    tmp.select();
-	document.execCommand("copy");
-    document.body.removeChild(tmp);
-    
-	alert("URL이 복사되었습니다"); 
+    navigator.cliboard.writeText(url).then(() => {
+	alert("URL이 복사되었습니다") 	   
+    })
 }
 
 shareButton.addEventListener('click', copyUrl);
